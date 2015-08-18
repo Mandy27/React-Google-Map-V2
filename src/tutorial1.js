9e2747444023d0ce5fcc3cbae87009d5caf15031
Mandy27 <giaman27@gmail.com>
2015-08-18 13:35:19 -0700

    Done

--- src/tutorial1.js
+++ src/tutorial1.js
@@ -24,23 +24,62 @@ var carArr = [{
 var CarList = React.createClass({
 	getInitialState: function(){
 		return {
-			selectedValue: "all"
+			selectedValue: "all",
+			cars: carArr
 		};
 	},
-	handeChange: function(e){
-		this.setState({selectedValue: e.target.value});
+	handleChange: function(e){
+		var select = e.target.value;
+		var cars = this.state.cars;
+		if(select == "truck"){
+			hideAllMarkers();
+			cars[0].options.visible = true;
+			this.setState({selectedValue: select, cars: cars});
+		}
+		else if(select == "sedan"){
+			hideAllMarkers();
+			cars[1].options.visible = true;
+			this.setState({selectedValue: select, cars: cars});
+		}
+		else if(select == "van"){
+			hideAllMarkers();
+			cars[2].options.visible = true;
+			this.setState({selectedValue: select, cars: cars});
+		}
+		else if(select == "motorcycle"){
+			hideAllMarkers();
+			cars[3].options.visible = true;
+			this.setState({selectedValue: select, cars: cars});
+		}
+		else{
+			showAllMarkers();
+			this.setState({selectedValue: select, cars: cars});
+		}
+		function showAllMarkers(){
+			for(var i = 0; i < cars.length; i++){
+	    		cars[i].options.visible = true;
+	    	}
+		}
+	    function hideAllMarkers() {
+	    	for(var i = 0; i < cars.length; i++){
+	    		cars[i].options.visible = false;
+	    	}
+	    }
 	},
   	render: function() {
   		return (
   			<div>
-	  			<label> Choose car: </label>
-	  			<select value={this.state.selectedValue} onChange={this.handeChange}>
-				  <option value="all">All</option>
-				  <option value="truck">Truck</option>
-				  <option value="sedan">Sedan</option>
-				  <option value="van">Van</option>
-				  <option value="motorcycle">Motorcycle</option>
-				</select>
+	  			<div id="carList">
+		  			<label> Choose car: </label>
+		  			<select value={this.state.selectedValue} onChange={this.handleChange}>
+					  <option value="all">All</option>
+					  <option value="truck">Truck</option>
+					  <option value="sedan">Sedan</option>
+					  <option value="van">Van</option>
+					  <option value="motorcycle">Motorcycle</option>
+					</select>
+				</div>
+				<Gmap cars={this.state.cars}></Gmap>
 			</div>
   		);
   	}
@@ -55,7 +94,7 @@ var Gmap = React.createClass({
     		zoom: 14
   		});
   		var markers = [];
-  		carArr.forEach(function(item){
+  		this.props.cars.forEach(function(item){
 			var marker = new google.maps.Marker({
 				id: item.id,
     			position: item.coords,
@@ -71,20 +110,22 @@ var Gmap = React.createClass({
 	},
 	moveMarkers: function(){
 		var markers = this.state.markers;
-		var move = function(markers){
-			 for(var i = 0; i < markers.length; i++){
+		setInterval(function(){
+			for(var i = 0; i < markers.length; i++){
 		     	var l = markers[i].getPosition().lat() + (Math.random() * (0.002 - 0.0001) + 0.0001) - 0.001;
 		     	var lo = markers[i].getPosition().lng()+ (Math.random() * (0.002 - 0.0001) + 0.0001) - 0.001;
 			    markers[i].setPosition(new google.maps.LatLng(l, lo));
      		}
-		}
-		var helper = function(markers){
-			return function(){
-				move(markers);
+		}, 1000);
+	},
+	componentWillReceiveProps:function(nextProps){			//Check if any props are changing
+		var markers = this.state.markers;
+		for(var i = 0; i < markers.length; i++){
+			if(nextProps.cars[i].options.visible != markers[i].visible){
+				markers[i].setVisible(nextProps.cars[i].options.visible);
 			}
 		}
-
-		setInterval(helper(markers), 1000);
+		this.setState({markers: markers});
 	},
   	render: function() {
   		var cx = React.addons.classSet;
@@ -93,10 +134,12 @@ var Gmap = React.createClass({
   			'markersMove': this.moveMarkers()
   		});
   		return (
-  			<div className={classes}></div>
+  			<div>
+  				<div className={classes}></div>
+  			</div>
   		);
   	}
 });
 
-React.render(<Gmap/>,document.getElementById("myMap"));
-React.render(<CarList/>,document.getElementById("carList"));
+// React.render(<Gmap/>,document.getElementById("myMap"));
+React.render(<CarList/>,document.getElementById("myMap"));
